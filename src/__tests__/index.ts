58846623c1baa07e5333dfbe9b43bbd7d5b88bbc
Nick Heiner <NickHeiner@users.noreply.github.com>
2020-09-19 18:50:06 -0700

    Migrate to TS (#4)
    Also fixes issue with yarn pointing to the Netflix internal registry.

---
+++ src/__tests__/index.ts
@@ -0,0 +1,39 @@
+import createLog, {constantizeLogEntryForTest} from '..';
+import {RingBuffer} from 'bunyan';
+
+// I may come to regret having this test directly in TS, as opposed to building the JS and then running the tests on 
+// that. But I think this will give a smoother dev experience.
+
+const wait = (timeMs: number) => new Promise(resolve => setTimeout(resolve, timeMs));
+
+// I'm ok with magic numbers in a test.
+/* eslint-disable no-magic-numbers */
+
+it('logger', async () => {
+  const ringBuffer = new RingBuffer({limit: 100});
+  const logger = createLog({
+    name: 'test-log', 
+    streams: [{
+      level: 'trace',
+      type: 'raw',
+      stream: ringBuffer
+    }],
+    stream: undefined
+  });
+  logger.trace({dataEntry: 'data value trace'}, 'String label');
+  logger.debug({dataEntry: 'data value debug'}, 'String label');
+  logger.info({dataEntry: 'data value info'}, 'String label');
+  logger.warn({dataEntry: 'data value warn'}, 'String label');
+  logger.error({dataEntry: 'data value error'}, 'String label');
+  logger.fatal({dataEntry: 'data value fatal'}, 'String label');
+
+  await logger.logPhase({phase: 'phase name'}, async (logProgress, setAdditionalLogMetadata) => {
+    await wait(10);
+    logProgress({substep: 1}, 'First wait complete');
+    await wait(10);
+    logProgress({substep: 2}, 'Second wait complete');
+    setAdditionalLogMetadata({additional: 'metadata'});
+  });
+
+  expect(ringBuffer.records.map(record => constantizeLogEntryForTest(record))).toMatchSnapshot();
+});
---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import bunyan from 'bunyan';
+import _ from 'lodash';
+import prettyMs from 'pretty-ms';
+import bunyanFormat from 'bunyan-format';
+
+const format = bunyanFormat({outputMode: 'short'});
+
+export type LogMetadata = {
+  level?: bunyan.LogLevelString
+} & Record<string, unknown>;
+
+export type LogPhaseMetadata = {phase: string} & LogMetadata;
+export type PhaseFunction<T> = (
+  logProgress: (metadata: Record<string, unknown>, label?: string) => void, 
+  setAdditionalLogData: (extraDataToSet: Record<string, unknown>) => void) => Promise<T>; 
+
+export type NTHLogger = {
+  logPhase<T>(logOpts: LogPhaseMetadata, fn: PhaseFunction<T>): Promise<T>;
+} & ReturnType<typeof bunyan.createLogger>;
+
+export default function createLogger(opts: Parameters<typeof bunyan.createLogger>[0]): NTHLogger {
+  const logger = bunyan.createLogger({
+    stream: format,
+    // Assume the user passed a valid loglevel.
+    level: process.env.loglevel as bunyan.LogLevelString,
+    ...opts
+  }) as NTHLogger;
+
+  logger.info();
+
+  async function logPhase<T>(logOpts: LogPhaseMetadata, fn: PhaseFunction<T>) {
+    const logOptsWithoutMetadata = _.omit(logOpts, 'phase', 'level'),
+      {phase, level = 'info'} = logOpts;
+    logger[level](logOptsWithoutMetadata, `Starting ${phase}`);
+
+    const startTime = new Date();
+
+    function getDurationStats() {
+      const endTime = new Date(),
+        durationMs = endTime.valueOf() - startTime.valueOf();
+  
+      return {durationMs, prettyDuration: prettyMs(durationMs)};
+    }
+  
+    function logProgress(logOpts: Record<string, unknown>) {
+      logger[level]({...logOpts, ...getDurationStats()}, `In progress: ${phase}`);
+    }
+  
+    let logOptsAdditions: Record<string, unknown> | undefined;
+    const returnVal = await fn(logProgress, additionalLogData => {
+      logOptsAdditions = additionalLogData; 
+    });
+  
+    logger[level](
+      {...logOptsWithoutMetadata, ...logOptsAdditions, ...getDurationStats()}, 
+      `Completed ${phase}`
+    );
+  
+    return returnVal;
+  }
+
+  return Object.assign(logger, {logPhase});
+}
+
+type LogEntry = {
+  hostname: string;
+  pid: number;
+  time: Date;
+  durationMs: number;
+  prettyDuration: string;
+} & Record<string, unknown>;
+
+export function constantizeLogEntryForTest(logEntry: LogEntry): LogEntry {
+  return {
+    ..._.omit(logEntry, 'pid', 'hostname', 'time', 'durationMs', 'prettyDuration'),
+    // It's over 9000.
+    pid: 9001,
+    
+    durationMs: 117,
+    prettyDuration: '<duration placeholder>',
+
+    hostname: '<hostname placeholder>',
+    time: new Date('2012'),
+  }
+}
